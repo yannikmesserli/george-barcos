53e9a20639150721fa605c14b1cda9d9cbe1a04f
Yannik Messerli <ymesserli@esri.com>
2024-01-12 14:29:47 +0100

    Static website based on old website

--- readme.md
+++
@@ -1,6 +0,0 @@
-# George-barcos.ch
-
-```
-cd george-barcos
-php -S localhost:8000
-```
--- site/accounts/admin.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-password: >
-  $2a$10$DFQGNZ4x6u1Uj1BLybEEzezxW.GCXUFiVm7zF0oW2uejOIE.4uVo2
-email: admin@admin.test
-language: en
-role: admin
